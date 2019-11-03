316cec37c38b88bc6a1674de3b4d79b0b951d859
fortisaqua <1336250263@qq.com>
2019-11-03 11:08:42 +0800

    first commit

---
+++ FollowCam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform targetTr;  // transform information of target to be followed by camera 
+    public float dist = 10.0f;  // distance between camera and target
+    public float height = 3.0f;  // height of camera
+    public float dampTrace = 20.0f;  // variable to guarantee smooth tracing
+    // public float lerpVar = 0.5f;
+
+    private Transform tr;  //  transform information of camera 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // LateUpdate Function will be called after Update Function called 
+    // this function will be called after player object finished transforming
+    private void LateUpdate()
+    {
+        // put camera behind player object with value of variable "dist"
+        // and pull up camera with value of variable "height"
+        // third variable of lerp function will eventually become 1 after player moved, and it started with Time.deltaTime * dampTrace
+        // Time.deltaTime * dampTrace defines a speed of moving from source position to target position
+        // calculation is : source += (target - source) * speed [source and target are Vector3 and speed is third variable in lerp function]
+        tr.position = Vector3.Lerp(tr.position, 
+                                    targetTr.position - (targetTr.forward * dist) + (Vector3.up * height),
+                                    Time.deltaTime * dampTrace);
+        Debug.Log("camera position: " + tr.position.ToString());
+        Debug.Log("player position: " + targetTr.position.ToString());
+        // let camera look at player object
+        tr.LookAt(targetTr.position);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private Transform tr;
+    public float moveSpeedH = 10.0f;
+    public float moveSpeedV = 10.0f;
+    public float rotSpeed = 50.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("H=" + h.ToString());
+        Debug.Log("V=" + v.ToString());
+        Debug.Log("forward: " + Vector3.forward.ToString() + "  right: " + Vector3.right.ToString() + "  up: " + Vector3.up.ToString());
+        Vector3 Direction = Vector3.forward * moveSpeedV * v + Vector3.right * moveSpeedH * h;
+        tr.Translate(Direction.normalized * Time.deltaTime, Space.Self);
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+    }
+}
